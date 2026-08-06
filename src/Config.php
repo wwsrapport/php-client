670916a00038d01b8c6319cc33191af54b75e709
Martin <martin@MacBook-Pro-van-Martin.local>
2026-08-06 17:20:51 +0200

    chore: release client version 0.2.0

--- src/Config.php
+++ src/Config.php
@@ -9,7 +9,7 @@ final class Config
     public function __construct(
         public readonly string $apiKey,
         public readonly string $baseUrl = 'https://wwsrapport.nl/v1',
-        public readonly string $userAgent = 'wwsrapport-php-client/0.1.0',
+        public readonly string $userAgent = 'wwsrapport-php-client/0.2.0',
     ) {
         if ($this->apiKey === '') {
             throw new \InvalidArgumentException('The WWSrapport API key may not be empty.');
